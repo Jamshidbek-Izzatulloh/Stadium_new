220b93b0876f64eab0183919d5a1c3e3af06207c
oybekmd <oybekatamatov999@gmail.com>
2023-07-28 09:47:44 +0500

    user and owner models

---
+++ stadium/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from datetime import datetime
+
+class OwnerModel(models.Model):
+    name = models.CharField(max_length=100, default='')
+    lname = models.CharField(max_length=100, default='')
+    date_of_birth = models.DateField(default=datetime.now)
+    address = models.CharField(max_length=150)
+    email = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to="Owner_photo/%Y/%m/%d", blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        db_table = 'stadium_owner_model'
+
+
+class StadiumModel(models.Model):
+    name = models.CharField(max_length=100, default='')
+    address = models.CharField(max_length=500, default='')
+    contact = models.CharField(max_length=30, default='')
+    photo = models.ImageField(upload_to="stadium_photo/%Y/%m/%d", blank=True)
+    price1hour = models.IntegerField(default=0)
+    owner = models.ForeignKey(OwnerModel, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        db_table = 'stadium_model'
+
+
+class BronedStadiumModel(models.Model):
+    from users.models import BronStadiumModel
+    name = models.ForeignKey(BronStadiumModel, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        db_table = 'broned_stadium_model'
+
+
+
+
+
+
+
+
+
+
+
